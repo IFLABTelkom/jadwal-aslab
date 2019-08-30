61d5ec07dd2d9924044e4d988090a4333f7977ec
fikriaksan <fikriaksan17@gmail.com>
2019-08-30 13:18:58 +0700

    Update Jadwal & Daftar Aslab

--- src/JadwalAslab.js
+++ src/JadwalAslab.js
@@ -1,97 +1,92 @@
 const jadwalAslab = {
   senin: {
     jam: {
-      6: ['KHM', 'AFG', 'VIN'],
-      7: ['KHM', 'AFG', 'VIN'],
-      8: ['KHM', 'AFG', 'VIN'],
-      9: ['KHM', 'AFG', 'VIN'],
-      10: ['KHM', 'AFG', 'VIN'],
-      11: ['KHM', 'AFG', 'VIN'],
-      12: ['VIN', 'AFG', 'RAH'],
-      13: ['VIN', 'AFG', 'RAH'],
-      14: ['VIN', 'AFG', 'RAH'],
-      15: ['KHM', 'AFG', 'RAH'],
-      16: ['KHM', 'AFG', 'RAH'],
-      17: ['KHM', 'AFG', 'RAH'],
-      18: ['KHM', 'AFG', 'RAH'],
-      19: ['KHM', 'AFG', 'RAH'],
-      20: ['KHM', 'AFG', 'RAH'],
-      21: ['KHM', 'AFG', 'RAH']
+      8: ['RMT', 'GRS', 'CRM'],
+      9: ['RMT', 'GRS', 'CRM'],
+      10: ['RMT', 'GRS', 'CRM'],
+      11: ['RMT', 'GRS', 'CRM'],
+      12: ['FAM', 'RMT', 'CRM'],
+      13: ['FAM', 'FER', 'CRM'],
+      14: ['FAM', 'FER', 'CRM'],
+      15: ['FAM', 'FER', 'HEV'],
+      16: ['FAM', 'FER', 'HEV'],
+      17: ['FAM', 'FER', 'HEV'],
+      18: ['FAM', 'FER', 'HEV']
     }
   },
   selasa: {
     jam: {
-      6: ['AFG', 'RAH', 'KHM'],
-      7: ['AFG', 'RAH', 'KHM'],
-      8: ['AFG', 'RAH', 'KHM'],
-      9: ['AFG', 'RAH', 'KHM'],
-      10: ['AFG', 'RAH', 'KHM'],
-      11: ['AFG', 'RAH', 'HRD'],
-      12: ['AFG', 'RAH', 'HRD'],
-      13: ['AFG', 'VIN', 'HRD'],
-      14: ['AFG', 'VIN', 'HRD'],
-      15: ['AFG', 'VIN', 'HRD'],
-      16: ['AFG', 'VIN', 'HRD'],
-      17: ['AFG', 'VIN', 'HRD']
+      6: ['FAM', 'GRS', 'CRM'],
+      7: ['FAM', 'GRS', 'CRM'],
+      8: ['FAM', 'GRS', 'CRM'],
+      9: ['FAM', 'GRS', 'CRM'],
+      10: ['RMT', 'FER', 'CRM'],
+      11: ['RMT', 'FER', 'CRM'],
+      12: ['RMT', 'FER', 'CRM'],
+      13: ['RMT', 'FER', 'HEV'],
+      14: ['RMT', 'FER', 'HEV'],
+      15: ['RMT', 'FER', 'HEV'],
+      16: ['FAM', 'RMT', 'HEV'],
+      17: ['FAM', 'RMT', 'HEV'],
+      18: ['FAM', 'RMT', 'HEV']
     }
   },
   rabu: {
     jam: {
-      6: ['RAH', 'VIN', 'KHM'],
-      7: ['RAH', 'VIN', 'KHM'],
-      8: ['RAH', 'VIN', 'KHM'],
-      9: ['RAH', 'VIN', 'KHM'],
-      10: ['RAH', 'VIN', 'KHM'],
-      11: ['RAH', 'VIN', 'KHM'],
-      12: ['AFG', 'VIN', 'KHM'],
-      13: ['AFG', 'VIN', 'KHM'],
-      14: ['AFG', 'VIN', 'KHM'],
-      15: ['AFG', 'VIN', 'KHM'],
-      16: ['AFG', 'VIN', 'KHM'],
-      17: ['AFG', 'VIN', 'KHM']
+      6: ['FAM', 'GRS', 'HEV'],
+      7: ['FAM', 'GRS', 'HEV'],
+      8: ['FAM', 'GRS', 'HEV'],
+      9: ['FER', 'GRS', 'HEV'],
+      10: ['FER', 'GRS', 'HEV'],
+      11: ['FER', 'GRS', 'HEV'],
+      12: ['FAM', 'FER', 'HEV'],
+      13: ['FAM', 'FER', 'CRM'],
+      14: ['FAM', 'FER', 'CRM'],
+      15: ['FAM', 'FER', 'CRM'],
+      16: ['FAM', 'FER', 'CRM'],
+      17: ['FAM', 'FER', 'CRM'],
+      18: ['FAM', 'FER', 'CRM']
     }
   },
   kamis: {
     jam: {
-      6: ['RAH', 'AFG', 'KHM'],
-      7: ['RAH', 'AFG', 'KHM'],
-      8: ['RAH', 'AFG', 'KHM'],
-      9: ['RAH', 'AFG', 'KHM'],
-      10: ['RAH', 'AFG', 'KHM'],
-      11: ['RAH', 'AFG', 'KHM'],
-      12: ['RAH', 'AFG', 'KHM'],
-      13: ['RAH', 'VIN', 'KHM'],
-      14: ['RAH', 'VIN', 'KHM'],
-      15: ['RAH', 'VIN', 'HRD'],
-      16: ['RAH', 'VIN', 'HRD'],
-      17: ['RAH', 'VIN', 'HRD']
+      6: ['FAM', 'RMT', 'CRM'],
+      7: ['FAM', 'RMT', 'CRM'],
+      8: ['FAM', 'RMT', 'CRM'],
+      9: ['RMT', 'GRS', 'CRM'],
+      10: ['RMT', 'GRS', 'HEV'],
+      11: ['RMT', 'GRS', 'HEV'],
+      12: ['FER', 'GRS', 'HEV'],
+      13: ['FER', 'GRS', 'HEV'],
+      14: ['FER', 'GRS', 'HEV'],
+      15: ['FAM', 'FER', 'HEV'],
+      16: ['FAM', 'FER', 'HEV'],
+      17: ['FAM', 'FER', 'HEV'],
+      18: ['FAM', 'FER', 'HEV']
     }
   },
   jumat: {
     jam: {
-      0: ['RAH', 'AFG', 'VIN'],
-      1: ['RAH', 'AFG', 'VIN'],
-      2: ['RAH', 'AFG', 'VIN'],
-      3: ['RAH', 'AFG', 'VIN'],
-      4: ['RAH', 'AFG', 'VIN'],
-      5: ['RAH', 'AFG', 'VIN'],
-      6: ['RAH', 'AFG', 'VIN'],
-      7: ['RAH', 'AFG', 'VIN'],
-      8: ['RAH', 'AFG', 'VIN'],
-      9: ['RAH', 'AFG', 'VIN'],
-      10: ['RAH', 'AFG', 'VIN'],
-      13: ['RAH', 'KHM', 'VIN'],
-      14: ['RAH', 'KHM', 'VIN'],
-      15: ['RAH', 'KHM', 'VIN'],
-      16: ['RAH', 'KHM', 'VIN'],
-      17: ['RAH', 'KHM', 'VIN']
+      7: ['FAM', 'GRS', 'HEV'],
+      8: ['FAM', 'GRS', 'HEV'],
+      9: ['FAM', 'GRS', 'HEV'],
+      10: ['FAM', 'GRS', 'HEV'],
+      11: ['FAM', 'GRS', 'HEV'],
+      15: ['RMT', 'GRS', 'HEV'],
+      16: ['RMT', 'GRS', 'HEV'],
+      17: ['RMT', 'GRS', 'HEV'],
+      18: ['RMT', 'GRS', 'HEV']
     }
   },
   sabtu: {
     jam: {
-      12: ['MFS', 'GSU', 'MFA'],
-      13: ['MFS', 'GSU', 'MFA'],
-      14: ['MFS', 'GSU', 'MFA']
+      6: ['RMT', 'GRS', 'CRM'],
+      7: ['RMT', 'GRS', 'CRM'],
+      8: ['RMT', 'GRS', 'CRM'],
+      9: ['RMT', 'FER', 'CRM'],
+      10: ['RMT', 'FER', 'CRM'],
+      11: ['RMT', 'FER', 'CRM'],
+      12: ['RMT', 'FER', 'CRM']
     }
   }
 };
